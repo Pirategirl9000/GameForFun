820d0137f91a6f6bbb3696ea259d0fd7462754a7
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2024-09-15 02:28:54 +0000

    ff

--- script.js
+++ script.js
@@ -8,20 +8,77 @@ class Disp {
     }
 
     cls() {
-        ctx.beginPath();
-        ctx.fillStyle = "black";
-        ctx.fillRect(0, 0, this.WIDTH, this.HEIGHT);
-        ctx.fill();
-        ctx.closePath();
+        this.ctx.beginPath();
+        this.ctx.fillStyle = "black";
+        this.ctx.fillRect(0, 0, this.WIDTH, this.HEIGHT);
+        this.ctx.fill();
+        this.ctx.closePath();
     }
 }
 
-disp = Disp()
+var disp = new Disp();
+
+class Player {
+    constructor() {
+        this.x = disp.WIDTH/2;
+        this.y = 100;
+        this.dirX = 0;
+        this.speed = 3;
+        
+    }
+
+    update() {
+        if (this.x < -20) {
+            this.x = disp.WIDTH;
+        } else if (this.x > disp.WIDTH + 20) {
+            this.x = 0;
+        }
+        this.x += this.dirX * this.speed;
+        //this.y += 
+
+        this.draw();
+    }
+
+    draw() {
+        disp.ctx.beginPath();
+        disp.ctx.fillStyle = 'red';
+        disp.ctx.fillRect(this.x, this.y, 20, 20);
+        disp.ctx.fill();
+        disp.ctx.closePath();
+    }
+
+
+}
+
+player = new Player();
+
+
+
+let alive = setInterval( () => {
+    disp.cls();
+    player.update();
+
+}, 17);
+
+
+
 
 
 
 document.addEventListener('keydown', (e) => {
     if (e.code == 'KeyF') {
-        document.getElementById("display").requestFullscreen()
+        disp.element.requestFullscreen()
     }
-})
+
+    if (e.code == 'KeyD') {
+        player.dirX = 1;
+    } else if (e.code == 'KeyA') {
+        player.dirX = -1;
+    }
+});
+
+document.addEventListener('keyup', (e) => {
+    if (e.code == 'KeyD' || e.code == 'KeyA') {
+        player.dirX = 0;
+    }
+});
