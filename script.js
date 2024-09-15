7416d4507707e0b9fa736a5bd905b55964f11f7f
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2024-09-15 02:45:59 +0000

    ff

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 class Disp {
+    /**
+     * Gets reference to canvas named 'display'
+     */
     constructor() {
         this.element = document.getElementById("display");
         this.ctx = document.getElementById("display").getContext('2d');
         this.WIDTH = 1280;
         this.HEIGHT = 720;
-        this.cls();
+        this.cls(); //sets background
     }
 
+    /**
+     * Clears Canvas
+     */
     cls() {
         this.ctx.beginPath();
         this.ctx.fillStyle = "black";
@@ -19,21 +25,29 @@ class Disp {
 var disp = new Disp();
 
 class Player {
+    /**
+     * Creates a new instance of player
+     */
     constructor() {
         this.x = disp.WIDTH/2;
         this.y = 100;
         this.left = 0;
         this.right = 0;
         this.speed = 3;
-        
     }
 
+    /**
+     * Updates the players position and draws the player to ctx
+     */
     update() {
         this.move();
         this.draw();
     }
 
-    move() {
+    /**
+     * Moves the player
+     */
+    static move() {
         if (this.x < -20) {
             this.x = disp.WIDTH;
         } else if (this.x > disp.WIDTH + 20) {
@@ -42,6 +56,9 @@ class Player {
         this.x += this.left * this.speed + this.right * this.speed;
     }
 
+    /**
+     * Draws player object
+     */
     draw() {
         disp.ctx.beginPath();
         disp.ctx.fillStyle = 'red';
@@ -55,12 +72,9 @@ class Player {
 
 var player = new Player();
 
-
-
 let alive = setInterval( () => {
     disp.cls();
     player.update();
-
 }, 17);
 
 
